47962247994f28f5a1fd899fde64c32195f92e75
KHIZAR0706 <giyoskhujanazarov@gmail.com>
2025-06-02 22:36:32 +0900

    feat: develop master router ejs page

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if(err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data)
+  }
+});
 
 // 1: Entry code
 app.use(express.static("public"));
@@ -25,6 +36,10 @@ app.set("view engine", "ejs");
 app.post("/create-item", (req, res) => {    // POST => Malumotni ozi bilan olib keladi va osha malumotni DATABASE ga yozadi!
     console.log(req.body);
     res.json({test: "success"});
+});
+
+app.get('/master', (req, res) => {
+  res.render("master", {user: user});
 });
 
 app.get("/", function(req, res) {    // GET => DATABASE dan malumotni olish yoki oqish uchun ishlatiladi!
