3482e95f42e7cad61316949184e6c36db9ff1244
KHIZAR0706 <giyoskhujanazarov@gmail.com>
2025-06-05 07:05:39 +0900

    feat :train callback and async functions

--- train.js
+++ train.js
@@ -1,2 +1,151 @@
 console.log("TRAIN AREA!");
-// D-TASK
+console.log("***************");
+
+
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//    "yaxshi talaba bo'ling", // 0-20
+//    "to'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
+//    "o'zingizni ishlashingizni boshlang", // 30-40
+//    "siz kuchli bo'lgan narsalarni qiling", // 40-50
+//    "yoshlarga investitsiya qiling", // 50-60
+//    "endi dam oling, foydasi yoq endi", // 60
+// ];
+
+// NodeJS event loop va Callback functionlarni o'rganamiz! => CALLBACK FUNCTIONS
+
+//  function maslahatBering(a, callback) {
+//  if(typeof a !== 'number') callback ("insert a number", null);
+//  else if(a <= 20)  callback(null, list[0]);
+//  else if(a > 20 && a <= 30) callback(null, list[1]);
+//  else if(a > 30 && a <= 40) callback(null, list[2]);
+//  else if(a > 40 && a <= 50) callback(null, list[3]);
+//  else if(a > 50 && a <= 60) callback(null, list[4]);
+//  else{
+//    setInterval(() => {
+//    callback(null, list[5]);
+//     }, 1000);
+
+//    setTimeout(function () {
+//       callback(null, list[5]);
+//        }, 5000)
+//      }
+//   }
+
+// maslahatBering(10, (err, data) => {
+//  if(err) console.log('ERROR:', err);
+//   console.log('javob:', data);
+//  })
+
+
+// maslahatBering(26, (err, data) => {
+//  if(err) console.log('ERROR:', err);
+//   console.log('javob:', data);
+//  })
+
+
+// maslahatBering(38, (err, data) => {
+//  if(err) console.log('ERROR:', err);
+//   console.log('javob:', data);
+//  })
+
+
+// maslahatBering(49, (err, data) => {
+//  if(err) console.log('ERROR:', err);
+//   console.log('javob:', data);
+//  })
+
+
+// maslahatBering(55, (err, data) => {
+//  if(err) console.log('ERROR:', err);
+//   console.log('javob:', data);
+//  })
+
+
+// maslahatBering(61, (err, data) => {
+//  if(err) console.log('ERROR:', err);
+//   console.log('javob:', data);
+//  })
+
+
+// maslahatBering("salom", (err, data) => {
+//   if(err) console.log('ERROR:', err);
+//   else{
+//     console.log('javob:', data);
+//   }
+   
+// });
+
+// console.log('passed  here 0');
+// maslahatBering(65, (err, data) => {
+//  if(err) console.log('ERROR:', err);
+//    else{
+//  console.log('javob:', data);
+//  }
+   
+//  });
+//  console.log('passed here 1');
+
+
+// ASYNC FUNCTIONS!
+
+// async function maslahatBering(a) {
+//     if (typeof a !== "number") throw new Error("insert a number");
+//     else if (a <= 20) return list[0];
+//     else if (a > 20 && a <= 30) return list[1];
+//     else if (a > 30 && a <= 40) return list[2];
+//     else if (a > 40 && a <= 50) return list[3];
+//     else if (a > 50 && a <= 60) return list[4];
+//     else{
+//        return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//           resolve(list[5]);
+//        }, 5000);
+//      });
+//   }
+// }
+
+//  CALL via Then & Catch!
+
+// console.log("passed here 0");
+// maslahatBering(20)
+//   .then(data => {
+//     console.log('javob:', data);
+//   })
+//   .catch(err => {
+//    console.log('ERROR:', err); 
+//   });
+// console.log("passed here 1");
+
+
+// CALL via Async & Await!
+
+//  async function run() {
+//    let javob = await maslahatBering(20);
+//    console.log(javob);
+//    javob = await maslahatBering(70);
+//    console.log(javob);
+//    javob = await maslahatBering(41);
+//    console.log(javob);
+// }
+// run();
+
+// A-TASK
+/* Masalani izohi:
+Shunday 2 parametrli function tuzing, hamda 1-parametrdagi letterni, 2-parametrdagi word qatnashgan sonini return qilishi kerak. MASALAN: countLetter("e", "engineer") 3 sonini return qiladi.
+*/
+
+// Masalani yechimi:
+
+function countLetter(letter, word) {
+    let count = 0;
+    for(let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+            count++;
+        }
+    }
+    return count;
+}
+const result = countLetter("e", "engineer");
+console.log("result:", result);
