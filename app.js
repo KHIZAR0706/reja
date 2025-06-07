d7c4c41a25e467dc6bf5c16a5427f13462c7cc75
KHIZAR0706 <giyoskhujanazarov@gmail.com>
2025-06-07 17:52:05 +0900

    feat: mongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,52 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if(err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data)
+  }
+});
+
+// MongoDB call
+const db = require("./server").db();
+
+// 1: Entry code  
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2: Session code
+
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: Routing code
+// app.get("/hello", function(req, res) {
+// res.end(`<h1 style="background: green">HELLO WORLD by KHIZAR</h1>`);
+// });
+// app.get("/gift", function(req, res) {
+//  res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+// });
+
+app.post("/create-item", (req, res) => {    // POST => Malumotni ozi bilan olib keladi va osha malumotni DATABASE ga yozadi!
+    console.log(req.body);
+    res.json({test: "success"});
+});
+
+// app.get('/master', (req, res) => {
+//  res.render("master", {user: user});
+// });
+
+app.get("/", function(req, res) {    // GET => DATABASE dan malumotni olish yoki oqish uchun ishlatiladi!
+    res.render("reja");
+});
+
+module.exports = app;    
+       
+ 
