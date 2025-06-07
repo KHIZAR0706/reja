be540e73cb91fa64dc7748a8ddda14e36b0c3ba1
KHIZAR0706 <giyoskhujanazarov@gmail.com>
2025-06-07 17:51:10 +0900

    feat: mongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,53 +1,30 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if(err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data)
-  }
-});
+let db;
+const connectionSring ="mongodb+srv://k11973754:0MpJZ4LvLzPqFn5V@cluster0.nshrdgr.mongodb.net/Reja"
 
-// 1: Entry code  
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+mongodb.connect(
+    connectionSring,
+     {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+    },(err, client) => {
+     if(err) console.log("ERROR on connection MongoDB"); 
+     else {
+        console.log("MongoDB connection succeed");
+        module.exports = client;
+        
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function() {
+            console.log(
+                `The server is running successfully on port: ${PORT}, http://localhost: ${PORT}`
+            );
+        });
+      }
+     }  
+    );
 
-// 2: Session code
 
-// 3: Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4: Routing code
-// app.get("/hello", function(req, res) {
-// res.end(`<h1 style="background: green">HELLO WORLD by KHIZAR</h1>`);
-// });
-// app.get("/gift", function(req, res) {
-//  res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
-// });
-
-app.post("/create-item", (req, res) => {    // POST => Malumotni ozi bilan olib keladi va osha malumotni DATABASE ga yozadi!
-    console.log(req.body);
-    res.json({test: "success"});
-});
-
-app.get('/master', (req, res) => {
-  res.render("master", {user: user});
-});
-
-app.get("/", function(req, res) {    // GET => DATABASE dan malumotni olish yoki oqish uchun ishlatiladi!
-    res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function(){
-  console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-});    
-      
